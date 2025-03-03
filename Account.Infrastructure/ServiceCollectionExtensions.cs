afec4a7d4e6582b336adc5bcd6f15d1080f4622e
Giuseppe D'Amico <giuseppedamico84@gmail.com>
2025-03-03 10:58:21 +0100

    Ceate scaffolding

---
+++ Account.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Account.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
