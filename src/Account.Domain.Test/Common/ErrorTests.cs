a1a8bad8a02cba969a003cf83ea95ddc6d2ae225
Giuseppe D'Amico <giuseppedamico84@gmail.com>
2025-03-04 13:59:52 +0100

    Add Result classes

---
+++ src/Account.Domain.Test/Common/ErrorTests.cs
@@ -0,0 +1,72 @@
+﻿using Account.Domain.Common;
+using FluentAssertions;
+
+namespace Account.Domain.Test.Common
+{
+    public class ErrorTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializePropertiesCorrectly()
+        {
+            // Act
+            var error = new Error("ErrorCode", "Domain", "Description");
+
+            // Assert
+            error.Code.Should().Be("ErrorCode");
+            error.Domain.Should().Be("Domain");
+            error.Description.Should().Be("Description");
+        }
+
+        [Fact]
+        public void Constructor_WithNullDomainAndDescription_ShouldInitializeCorrectly()
+        {
+            // Act
+            var error = new Error("ErrorCode");
+
+            // Assert
+            error.Code.Should().Be("ErrorCode");
+            error.Domain.Should().BeNull();
+            error.Description.Should().BeNull();
+        }
+
+        [Fact]
+        public void ToString_ShouldReturnCorrectString()
+        {
+            // Act
+            var error = new Error("ErrorCode", "Domain", "Description");
+
+            // Assert
+            error.ToString().Should().Be("Code: ErrorCode, Domain: Domain, Description: Description");
+        }
+
+        [Fact]
+        public void ToString_WithNullDomain_ShouldReturnCorrectString()
+        {
+            // Act
+            var error = new Error("ErrorCode", null, "Description");
+
+            // Assert
+            error.ToString().Should().Be("Code: ErrorCode, Description: Description");
+        }
+
+        [Fact]
+        public void ToString_WithNullDescription_ShouldReturnCorrectString()
+        {
+            // Act
+            var error = new Error("ErrorCode", "Domain", null);
+
+            // Assert
+            error.ToString().Should().Be("Code: ErrorCode, Domain: Domain");
+        }
+
+        [Fact]
+        public void ToString_WithNullDomainAndDescription_ShouldReturnCorrectString()
+        {
+            // Act
+            var error = new Error("ErrorCode");
+
+            // Assert
+            error.ToString().Should().Be("Code: ErrorCode");
+        }
+    }
+}
---
+++ src/Account.Domain/Common/Error.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Account.Domain.Common
+{
+    public readonly struct Error
+    {
+        public Error(string code, string? domain = null, string? description = null)
+        {
+            Code = code;
+            Domain = domain;
+            Description = description;
+        }
+
+        public string Code { get; }
+
+        public string? Domain { get; }
+
+        public string? Description { get; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"Code: {Code}");
+            if (Domain != null)
+                sb.Append($", Domain: {Domain}");
+            if (Description != null)
+                sb.Append($", Description: {Description}");
+            return sb.ToString();
+        }
+    }
+}
